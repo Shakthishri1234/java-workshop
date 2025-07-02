609c0ab88dc2f9180b825cd1f7a30b19efec8d3c
shakthi123 <shakthishriaishwaryaasr24it@vetias.ac.in>
2025-07-02 15:03:40 +0530

    marks program

---
+++ src/com.vetias java-workshop/java/src/com/vetias/marks.java
@@ -0,0 +1,13 @@
+package src.com.vetias;
+
+public class marks {
+    public static void main(String[]args) {
+
+     int[]marks = {90,60,70,30,40};
+     for(int mark:marks) {
+     System.out.println(mark);
+    }
+}
+
+
+}
